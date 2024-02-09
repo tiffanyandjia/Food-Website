b8b0cd12795d33698761fd5af1d3684936b208eb
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-02-09 18:37:57 -0500

    mongoDB

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -16,4 +16,27 @@
     }
  }
 
+ export const createPost= async(req, res) => {
+    const { title, message, selectedFile, creator, tags} = req.body;
+
+    const newPostMessage = new PostMessage({title, message, selectedFile, creator, tags})
+
+    try {
+        await newPostMessage.save();
+        res.status(201).json(newPostMessage);
+    } catch (error) {
+        req.status(409).json({message:error.message});
+    }
+ }
+
+ export const updatePost = async (req, res) => {
+    const {id} = req.params;
+    const {title, message, creator, selectedFile, tags} = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatePost = { creator, title, message, tages, selectedFile, _id: id};
+    await PostMessage.findByIdAndUpdate(id, updatePost,)
+ }
+
  export default router;
